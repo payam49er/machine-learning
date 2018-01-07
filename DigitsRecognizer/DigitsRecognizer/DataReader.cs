1573a6fb859922d055bb48f2fd4975e54fccbb49
payam shoghi <payam49er@gmail.com>
2018-01-07 11:04:33 -0500

    initial commit

---
+++ DigitsRecognizer/DigitsRecognizer/DataReader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace DigitsRecognizer
+{
+    public class DataReader
+    {
+	    private static Observation ObservationFactory(string data)
+	    {
+		    var commaSeperatedValues = data.Split(',');
+		    var label = commaSeperatedValues[0];
+		    var pixels = commaSeperatedValues.Skip(1).Select(x => Convert.ToInt32(x)).ToArray();
+		    return new Observation(label, pixels);
+	    }
+
+	    public static Observation[] ReadObservations(string dataPath)
+	    {
+		    var data = File.ReadAllLines(dataPath).Skip(1).Select(ObservationFactory).ToArray();
+
+		    return data;
+	    }
+	}
+}
---
+++ DigitsRecognizer/DigitsRecognizer/Observation.cs
@@ -0,0 +1,13 @@
+﻿namespace DigitsRecognizer
+{
+    public class Observation
+    {
+	    public string Label { get; private set; }
+	    public int[] Pixels { get; private set; }
+	    public Observation(string label, int[] pixels)
+	    {
+		    Label = label;
+		    Pixels = pixels;
+	    }
+	}
+}
